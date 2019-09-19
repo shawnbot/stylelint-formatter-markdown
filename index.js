38286843fa5b258fe63f19961a1681875f1afbeb
Shawn Allen <shawn.allen@github.com>
2019-09-19 15:59:00 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const {MarkdownTable, stripCwd, getHeadRef, getRepoURL} = require('stylelint-formatter-utils')
+
+module.exports = function stylelintFormatterMarkdown(results) {
+  const repoURL = getRepoURL()
+  const headRef = getHeadRef()
+
+  const table = new MarkdownTable({
+    columns: [
+      {title: 'rule', format: ({rule}) => link(rule, `https://stylelint.io/user-guide/rules/${rule}`)},
+      {title: 'path', format: warning => link(warning.source, href(warning))},
+      {title: 'text', format: warning => warning.text}
+    ]
+  })
+
+  const rows = []
+  for (const {source, warnings} of results) {
+    for (const warning of warnings) {
+      warning.source = stripCwd(source)
+      rows.push(warning)
+    }
+  }
+
+  return table.format(rows)
+
+  function href({source, line}) {
+    const fragment = line ? `#L${line}` : ''
+    return `${repoURL}/blob/${headRef}/${source}${fragment}`
+  }
+
+  function link(text, url) {
+    return `[${text}](${url})`
+  }
+}
